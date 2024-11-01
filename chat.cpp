210342d6404c71635046c517faa187b89f54be75
Marinette Funk <marinettefunk@gmail.com>
2024-11-01 08:48:15 +0200

    Copyright commenting added to all files.

--- chat.cpp
+++ chat.cpp
@@ -1,3 +1,16 @@
+/*
+ * Copyright (c) 2024 Marinette Funk. All rights reserved.
+ *
+ * This file is part of the SYNAPSIS Project.
+ *
+ * Description: Virtual Assistant to help with file organisation, with ChatBot NLP functionality.
+ *
+ * Unauthorized copying of this file, via any medium, is strictly prohibited.
+ * Proprietary and confidential.
+ *
+ * For inquiries, contact: marinettefunk@gmail.com
+ */
+
 #include <iostream>
 #include <string>
 #include <algorithm>
